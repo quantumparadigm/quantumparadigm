edb82984ee79691268342f84f91131cb7abf89fd
quantumparadigm <Q.paradigm@hotmail.com>
2017-06-10 20:43:50 -0600

    Minor Changes

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,11 +1,3 @@
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
@@ -13,7 +5,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });
 });
